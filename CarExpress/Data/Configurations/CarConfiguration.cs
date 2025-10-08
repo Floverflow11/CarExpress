d79d7215e691adf6a4c32cb5de01964acb7fd500
Floverflow <flo.cinbolat@gmail.com>
2025-10-08 16:05:01 +0200

    feat: expand seed data to 7 cars and fix card grid layout

--- CarExpress/Data/Configurations/CarConfiguration.cs
+++ CarExpress/Data/Configurations/CarConfiguration.cs
@@ -38,6 +38,26 @@ public class CarConfiguration : IEntityTypeConfiguration<Car>
             {
                 Id = 3, Year = 2007, BoughtDate = new DateOnly(2022, 4, 4), BoughtPrice = 1800, RepairCost = 690,
                 CanBeSoldFromDate = new DateOnly(2022, 4, 8), IsAvailable = true, TrimId = 3
+            },
+            new Car
+            {
+                Id = 4, Year = 2017, BoughtDate = new DateOnly(2022, 4, 5), BoughtPrice = 24350, RepairCost = 1100,
+                CanBeSoldFromDate = new DateOnly(2022, 4, 9), IsAvailable = true, TrimId = 4
+            },
+            new Car
+            {
+                Id = 5, Year = 2008, BoughtDate = new DateOnly(2022, 4, 6), BoughtPrice = 4000, RepairCost = 475,
+                CanBeSoldFromDate = new DateOnly(2022, 4, 9), IsAvailable = true, TrimId = 5
+            },
+            new Car
+            {
+                Id = 6, Year = 2016, BoughtDate = new DateOnly(2022, 4, 6), BoughtPrice = 15250, RepairCost = 440,
+                CanBeSoldFromDate = new DateOnly(2022, 4, 10), IsAvailable = true, TrimId = 6
+            },
+            new Car
+            {
+                Id = 7, Year = 2013, BoughtDate = new DateOnly(2022, 4, 7), BoughtPrice = 10990, RepairCost = 950,
+                CanBeSoldFromDate = new DateOnly(2022, 4, 11), IsAvailable = true, TrimId = 7
             }
         );
     }
---
+++ CarExpress/Data/Migrations/20251007104040_SeedAdditionalCars.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarExpress.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdditionalCars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "BoughtDate", "BoughtPrice", "CanBeSoldFromDate", "Description", "IsAvailable", "IsSold", "RepairCost", "TrimId", "Vin", "Year" },
+                values: new object[,]
+                {
+                    { 4, new DateOnly(2022, 4, 5), 24350m, new DateOnly(2022, 4, 9), null, true, false, 1100m, 4, null, 2017 },
+                    { 5, new DateOnly(2022, 4, 6), 4000m, new DateOnly(2022, 4, 9), null, true, false, 475m, 5, null, 2008 },
+                    { 6, new DateOnly(2022, 4, 6), 15250m, new DateOnly(2022, 4, 10), null, true, false, 440m, 6, null, 2016 },
+                    { 7, new DateOnly(2022, 4, 7), 10990m, new DateOnly(2022, 4, 11), null, true, false, 950m, 7, null, 2013 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}

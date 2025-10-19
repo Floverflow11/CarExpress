e13dc6b16a237b19bf6b8c85eebb4c674c32de2c
Floverflow <flo.cinbolat@gmail.com>
2025-10-19 16:18:42 +0200

    feat: add inline data validation, layout and style changes in add car page

--- CarExpress/Models/CarAddViewModel.cs
+++ CarExpress/Models/CarAddViewModel.cs
@@ -1,3 +1,14 @@
-﻿namespace CarExpress.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record CarAddViewModel(decimal BoughtPrice, decimal RepairCost, int Year, string Brand, string Model, string Trim, IFormFile Image);
+namespace CarExpress.Models;
+
+public record CarAddViewModel(
+    [Required(ErrorMessage = "*")] decimal BoughtPrice,
+    [Required(ErrorMessage = "*")] decimal RepairCost,
+    [Required(ErrorMessage = "*")]
+    [Range(1990, 2025, ErrorMessage = "L'année doit être comprise entre 1990 et 2025.")]
+    int Year,
+    [Required(ErrorMessage = "*")] string Brand,
+    [Required(ErrorMessage = "*")] string Model,
+    [Required(ErrorMessage = "*")] string Trim,
+    [Required(ErrorMessage = "*")] IFormFile Image);
